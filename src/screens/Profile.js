a65475638e164a49ae4721bcc7b27e7359300dcd
zleppz <zleppz@uw.edu>
2019-11-11 13:38:41 -0800

    Stylesheet with avatar

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -6,18 +6,33 @@ import {View} from 'react-native';
 class Profile extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.body}>
 
   <Avatar
   rounded
+  size="large"
+  
   source={{
     uri:
       'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
   }}
+  onPress={() => console.log("Works!")}
+  activeOpacity={0.7}
+  containerStyle={{flex:0, marginLeft:100, marginTop: 0}}
 />
+
+
       </View>
     );
   }
 }
+var styles = StyleSheet.create({
+  body:{
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Profile;
