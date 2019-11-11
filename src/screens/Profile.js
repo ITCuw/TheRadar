99d48c958cf77865a9a517ae77eb940947f1eab6
ITCuw <alexdc2@uw.edu>
2019-11-11 13:37:58 -0800

    add sign out functionality to profile avatar

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -4,17 +4,24 @@ import { Avatar, Input, Button } from 'react-native-elements';
 import {View} from 'react-native';
 
 class Profile extends Component {
+
+  onSettingsClick(){
+    const {navigate} = this.props.navigation;
+    console.log('Settings Button Pressed');
+    navigate('ProfileSettings');
+  }
+
   render() {
     return (
       <View>
-
-  <Avatar
-  rounded
-  source={{
-    uri:
-      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-  }}
-/>
+          <Avatar
+            rounded
+            source={{
+              uri:
+                'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+            }}
+            onPress={()=> this.onSettingsClick()}
+          />
       </View>
     );
   }
